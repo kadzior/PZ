47fc30512f1401bc2d019f689020bd92f79d5dcb
kadzior <adriankadziolka18@gmail.com>
2024-10-20 23:03:12 +0200

    Update PZ1.cpp

--- PZ1/PZ1.cpp
+++ PZ1/PZ1.cpp
@@ -1,7 +1,36 @@
-﻿#include <iostream>
+#include <iostream>
+#include "DoublyLinkedList.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+        DoublyLinkedList list;
+
+        list.addToStart(10);
+        list.addToEnd(20);
+        list.addAtIndex(1, 15);
+        list.read();
+
+        list.removeFromStart();
+        list.read();
+
+        list.removeFromEnd();
+        list.read();
+
+        list.addToEnd(30);
+        list.addToEnd(40);
+        list.deleteAtIndex(1);
+        list.read();
+
+        list.readReverse();
+
+        list.printNextElement();
+        list.printNextElement();
+
+        list.printPrevElement();
+        list.printPrevElement();
+
+        list.clearList();
+        list.read();
+
+        return 0;
 }
-
